d5cd0ab47d58b32d0c91bdb508523a28495f1bb0
Eric Rowell <eric@platfora.com>
2014-09-17 17:58:35 -0700

    added project setup stuff and test directory

---
+++ server.js
@@ -0,0 +1,7 @@
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)();
+
+http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(1337);
---
+++ test/all-tests.js
@@ -0,0 +1,9 @@
+describe('#main', function(){
+  it('should observe all 2d context methods', function(){
+    var canvas = document.createElement('canvas'),
+        context = canvas.getContext();
+
+    assert.equal(-1, [1,2,3].indexOf(5));
+    assert.equal(-1, [1,2,3].indexOf(0));
+  })
+});

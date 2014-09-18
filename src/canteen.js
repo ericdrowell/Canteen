3221c2e9bd410ecd3579e214a4f11d16f73af288
Eric Rowell <eric@platfora.com>
2014-09-17 18:26:50 -0700

    updated license info

---
+++ src/canteen.js
@@ -0,0 +1,6 @@
+;(function() {
+  window.Canteen = {
+    version: '@VERSION',
+    date: '@DATE'
+  };
+})();
--- test/all-tests.js
+++ test/all-tests.js
@@ -5,5 +5,5 @@ describe('#main', function(){
 
     assert.equal(-1, [1,2,3].indexOf(5));
     assert.equal(-1, [1,2,3].indexOf(0));
-  })
+  });
 });

abcba23ab0688aa98d3a12d22f809c4f218c74d1
Felix Knischewski <fek@crosscan.com>
2017-07-19 13:43:42 +0200

    Disable stack size limit

--- src/canteen.js
+++ src/canteen.js
@@ -188,7 +188,7 @@
         arguments: Array.prototype.slice.call(args, 0)
       }); 
 
-      this._slice();
+      // this._slice();
     },
     /**
      * push instruction attribute onto the stack
@@ -203,7 +203,7 @@
         val: val
       }); 
 
-      this._slice();
+      //  this._slice();
     },
     /**
      * slice the stack if needed.  This means making sure that it doesn't exceed
